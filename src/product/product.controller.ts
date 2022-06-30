3e3bf4dba1acb0ed94d937e04729032d764ca7d7
dev.team <dev.team@gradify.app>
2022-06-30 11:40:34 +0430

    firstCommit

---
+++ src/product/product.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductDocument } from './product.schema';
+import { ProductService } from './product.service';
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Post()
+  createProduct(
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ): Promise<ProductDocument> {
+    return this.productService.create(name, price, description);
+  }
+}
